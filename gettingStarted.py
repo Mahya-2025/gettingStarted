e89d3e63c72ff72ff0c772291cfffe96333fa175
Mahya <mg8970@nyu.edu>
2025-09-15 19:16:36 -0400

    Add files via upload

---
+++ gettingStarted.py
@@ -0,0 +1,29 @@
+def welcome_assignment_answers(question):
+
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        import hashlib
+        email = "mg8970@nyu.edu"
+        answer = hashlib.sha256(email.encode()).hexdigest()
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 5
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
+    else:
+        answer = "Hmm... that question doesn't ring a bell. Could there be a typo?"
+    return answer
+
+if __name__ == "__main__":
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
